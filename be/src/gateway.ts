90756b0660e137b7ed5396be933d2d91300f8623
f.scassillo <f.scassillo@presstoday.com>
2023-08-30 14:35:57 +0200

    feat: points broadcast

--- be/src/gateway.ts
+++ be/src/gateway.ts
@@ -6,6 +6,7 @@ import { Server, Socket } from 'socket.io';
 export class GatewayService implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
     
   private cursorDataListener: { x: number, y: number, userId: string }[] = [];
+  private pointsData: any[] = [];
 
   @WebSocketServer() server: Server;
   private logger: Logger = new Logger('CursorGateway');
@@ -25,7 +26,14 @@ export class GatewayService implements OnGatewayInit, OnGatewayConnection, OnGat
   @SubscribeMessage('cursor')
   handleMessage(client: Socket, payload: { x: number, y: number, userId: string }): void {
     this.cursorDataListener.push(payload);
-    this.server.emit('cursor-update', this.cursorDataListener.filter(x => x.userId === payload.userId).slice(-10));
-    client.broadcast.emit('cursor-update', this.cursorDataListener.filter(x => x.userId === payload.userId).slice(-10));
+    this.server.emit('cursor-update', this.cursorDataListener.filter(x => x.userId === payload.userId).slice(-1));
+    client.broadcast.emit('cursor-update', this.cursorDataListener.filter(x => x.userId === payload.userId).slice(-1));
+  }
+
+  @SubscribeMessage('draw')
+  handleMessageDraw(client: Socket, payload: any[]): void {
+    this.pointsData = this.pointsData.concat(payload);
+    //this.server.emit('cursor-update', this.cursorDataListener.filter(x => x.userId === payload.userId).slice(-1));
+    client.broadcast.emit('draw-update', payload);
   }
 }
